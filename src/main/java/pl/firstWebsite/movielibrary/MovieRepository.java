81da80507b2ccced5a299e247b8571a53c2f8745
Rafal Osica <s21290@pjwstk.edu.pl>
2023-03-08 14:57:52 +0100

    Update 1

---
+++ src/main/java/pl/firstWebsite/movielibrary/MovieRepository.java
@@ -0,0 +1,21 @@
+package pl.firstWebsite.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public List<Movie> getAll(){
+        List<Movie> query = jdbcTemplate.query("Select * from movie", BeanPropertyRowMapper.newInstance(Movie.class));
+        return query;
+
+    }
+}
